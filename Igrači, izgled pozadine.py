56b26b04cdac0e9405258e6a7d99e5a54370a7aa
Lolitos <106661088+LoliVgim@users.noreply.github.com>
2023-12-10 18:48:40 +0100

    Update Igrači, izgled pozadine.py

--- Igrači, izgled pozadine.py
+++ Igrači, izgled pozadine.py
@@ -5,7 +5,7 @@ import sys
 pygame.init()
 
 # Postavke prozora
-width, height = 1920, 1080
+width, height = 1280, 700
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Stolni Tenis")
 
@@ -16,7 +16,7 @@ fps = 60
 # Postavke igrača
 player_radius = 40
 player_speed = 7
-player1_color = (0, 0, 255)  # Plava boja
+player1_color = (0, 255, 0)  # Zelena boja
 player2_color = (255, 0, 0)  # Crvena boja
 player1 = pygame.Rect(50, height // 2 - player_radius, 2 * player_radius, 2 * player_radius)
 player2 = pygame.Rect(width - 50 - player_radius * 2, height // 2 - player_radius, 2 * player_radius, 2 * player_radius)
@@ -28,9 +28,9 @@ ball = pygame.Rect(width // 2 - ball_size // 2, height // 2 - ball_size // 2, ba
 ball_direction = (0, 0)  # Početni smjer
 
 # Boje
-background_color = (0, 128, 0)  # Zelena boja pozadine
-element_color = (255, 255, 255)  # Boja mreže
-score_color = (255, 255, 255)  # Boja rezultata
+background_color = (255, 255, 255)  # Bijela boja pozadine
+element_color = (0, 0, 128)        # Tamnoplava boja mreže
+score_color = (255, 255, 255)      # Bijela boja rezultata
 
 # Serviranje
 serve_player1 = True
@@ -42,6 +42,9 @@ score_player2 = 0
 servis_count = 0
 max_servis_count = 2  # Broj servisa prije izmjene strane
 max_score = 11  # Maksimalan broj poena za pobjedu u igri
+
+# Dodatna postavka za provjeru servisa
+is_serving = False
 
 # Glavna petlja igre
 while True:
@@ -72,22 +75,24 @@ while True:
         player2.x += player_speed
 
     # Serviranje igrača 1
-    if keys[pygame.K_LCTRL] and serve_player1:
+    if keys[pygame.K_LCTRL] and serve_player1 and not is_serving:
         serve_player1 = False
         serve_player2 = True
         ball_direction = (1, -1)  # Lopta uvijek ide prema gornjem lijevom kutu
         ball.x = player1.right + 1  # Postavljanje lopte desno od igrača 1
         ball.y = player1.centery - ball_size // 2
         servis_count += 1
+        is_serving = True
 
     # Serviranje igrača 2
-    if keys[pygame.K_SPACE] and serve_player2:
+    if keys[pygame.K_SPACE] and serve_player2 and not is_serving:
         serve_player1 = True
         serve_player2 = False
         ball_direction = (-1, 1)  # Lopta uvijek ide prema donjem desnom kutu
         ball.x = player2.left - ball_size - 1  # Postavljanje lopte lijevo od igrača 2
         ball.y = player2.centery - ball_size // 2
         servis_count += 1
+        is_serving = True
 
     # Pomicanje lopte samo nakon servisa
     if ball_direction != (0, 0):
@@ -133,6 +138,7 @@ while True:
         ball_direction = (0, 0)  # Zaustavi loptu
         ball.x = width // 2 - ball_size // 2
         ball.y = height // 2 - ball_size // 2
+        is_serving = False
 
     elif ball.right >= width:
         score_player1 += 1
@@ -142,6 +148,7 @@ while True:
         ball_direction = (0, 0)  # Zaustavi loptu
         ball.x = width // 2 - ball_size // 2
         ball.y = height // 2 - ball_size // 2
+        is_serving = False
 
     # Izmjena strane nakon dva servisa
     if servis_count == max_servis_count:
@@ -158,36 +165,61 @@ while True:
     screen.fill(background_color)
     pygame.draw.circle(screen, player1_color, player1.center, player_radius)
     pygame.draw.circle(screen, player2_color, player2.center, player_radius)
-    pygame.draw.ellipse(screen, element_color, ball)
+    pygame.draw.ellipse(screen, (255, 165, 0), ball)  # Naranèasta boja lopte
 
-    # Crtanje mreže
+# Crtanje kruga u sredini ekrana
+    center_circle_radius = height // 4  # Half of the vertical line
+    center_circle_center = (width // 2, height // 2)
+    pygame.draw.circle(screen, element_color, center_circle_center, center_circle_radius, 5)
+
+# Crtanje paralelnih linija koje izlaze iz kruga
+    line_length = center_circle_radius * 2  # Full length of the vertical line
+    line_start = (center_circle_center[0], center_circle_center[1] - center_circle_radius)
+    line_end = (center_circle_center[0], center_circle_center[1] + center_circle_radius)
+    pygame.draw.line(screen, element_color, line_start, line_end, 5)
+
+# Crtanje na ekran
+    screen.fill(background_color)
+    pygame.draw.circle(screen, element_color, center_circle_center, center_circle_radius, 5)
+    pygame.draw.line(screen, element_color, line_start, line_end, 5)
+
+# Crtanje mreže
     pygame.draw.line(screen, element_color, (width // 2, 0), (width // 2, height), 10)
 
-    # Crtanje podebljane bijele horizontalne crte u sredini ekrana
+# Crtanje podebljane bijele horizontalne crte u sredini ekrana
     pygame.draw.line(screen, element_color, (0, height // 2), (width, height // 2), 4)
 
-    # Crtanje podebljanih linija uz rub prozora
+# Crtanje podebljanih linija uz rub prozora
     pygame.draw.line(screen, element_color, (0, 0), (0, height), 10)
     pygame.draw.line(screen, element_color, (width - 1, 0), (width - 1, height), 10)
-
-    # Crtanje podebljanih linija na kraju stola
-    pygame.draw.line(screen, element_color, (0, 0), (0, height), 10)
-    pygame.draw.line(screen, element_color, (width - 1, 0), (width - 1, height), 10)
-
-    # Crtanje paralelnih linija uz središnju horizontalnu crtu
     
+# Crtanje podebljanih linija na kraju stola
+    pygame.draw.line(screen, element_color, (0, 0), (0, height), 10)
+    pygame.draw.line(screen, element_color, (width - 1, 0), (width - 1, height), 10)
 
-    # Dodavanje horizontalnih linija uz rub stola
-    pygame.draw.line(screen, element_color, (0, 0), (width, 0), 5)
-    pygame.draw.line(screen, element_color, (0, height - 1), (width, height - 1), 5)
-    # Prikaz rezultata
+# Dodatne podebljane linije uz rub stola
+    pygame.draw.line(screen, element_color, (0, 0), (width, 0), 10)
+    pygame.draw.line(screen, element_color, (0, height - 1), (width, height - 1), 10)
+
+# Crtanje kruga iznad linija
+    pygame.draw.circle(screen, player1_color, player1.center, player_radius)
+    pygame.draw.circle(screen, player2_color, player2.center, player_radius)
+
+# Crtanje lopte
+    pygame.draw.ellipse(screen, (255, 165, 0), ball)  # Orange boja lopte
+
+# Prikaz rezultata
     font = pygame.font.Font(None, 36)
     score_text = font.render(f"{score_player1} - {score_player2}", True, element_color)
     screen.blit(score_text, (width // 2 - score_text.get_width() // 2, 10))
 
-    # Ažuriranje prozora
+# Ažuriranje prozora
     pygame.display.flip()
+
 
     # Postavljanje FPS-a
     clock.tick(fps)
+
+
+
 
